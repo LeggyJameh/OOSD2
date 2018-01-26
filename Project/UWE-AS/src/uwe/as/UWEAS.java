32f11110e12f1cbf8b2a53407844c7eee86ab72d
LordJameh <LordJameh@users.noreply.github.com>
2018-01-26 12:37:29 +0000

    Add primal functions to engine class

--- Project/UWE-AS/src/uwe/as/UWEAS.java
+++ Project/UWE-AS/src/uwe/as/UWEAS.java
@@ -22,33 +22,64 @@ public class UWEAS {
     
     public void addHall(Hall hall)
     {
-        
+        if (halls.contains(hall) == false)
+        {
+            halls.add(hall);
+        }
     }
     
     public void removeHall(Hall hall)
     {
-        
+        if (halls.contains(hall))
+        {
+            halls.remove(hall);
+        }
     }
     
     public void addUser(User user)
     {
-        
+        if (users.contains(user) == false)
+        {
+            users.add(user);
+        }
     }
     
     public void removeUser(User user)
     {
-        
+        if (users.contains(user))
+        {
+            users.remove(user);
+        }
     }
     
     public void addApplication(Application application)
     {
-        
+        if (applications.contains(application) == false)
+        {
+            applications.add(application);
+        }
     }
     
     public void removeApplication(Application application)
     {
-        
+        if (applications.contains(application))
+        {
+            applications.remove(application);
+        }
     }
     
+    public List<Hall> getHalls()
+    {
+        return halls;
+    }
     
+    public List<User> getUsers()
+    {
+        return users;
+    }
+    
+    public List<Application> getApplications()
+    {
+        return applications;
+    }
 }
