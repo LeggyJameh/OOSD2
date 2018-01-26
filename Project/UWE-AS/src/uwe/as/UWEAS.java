cbd3cb280f6d57ae408264846c82a69e6fe81605
LordJameh <LordJameh@users.noreply.github.com>
2018-01-26 10:23:21 +0000

    More init
    More init stuff. Need to design UI and core functions before proceeding

--- Project/UWE-AS/src/uwe/as/UWEAS.java
+++ Project/UWE-AS/src/uwe/as/UWEAS.java
@@ -1,21 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package uwe.as;
+
+import java.util.List;
 
 /**
  *
- * @author Jamie
+ * @author Adwait Chhetri (STUDENT NUMBER), George Jones (STUDENT NUMBER), Jamie Mills (16004255)
  */
 public class UWEAS {
 
+    List<Hall> halls;
+    List<User> users;
+    List<Application> applications;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+    
+    public void addHall(Hall hall)
+    {
+        
+    }
+    
+    public void removeHall(Hall hall)
+    {
+        
+    }
+    
+    public void addUser(User user)
+    {
+        
+    }
+    
+    public void removeUser(User user)
+    {
+        
+    }
+    
+    public void addApplication(Application application)
+    {
+        
+    }
+    
+    public void removeApplication(Application application)
+    {
+        
     }
     
 }
