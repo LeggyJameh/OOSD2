fe5a75a06d70fe9b96ad94533061d6b08e0e5478
LordJameh <LordJameh@users.noreply.github.com>
2018-01-26 13:01:52 +0000

    Instantiation and comments on engine class

--- Project/UWE-AS/src/uwe/as/UWEAS.java
+++ Project/UWE-AS/src/uwe/as/UWEAS.java
@@ -2,24 +2,35 @@
 package uwe.as;
 
 import java.util.List;
+import java.util.ArrayList;
 
 /**
  *
- * @author Adwait Chhetri (STUDENT NUMBER), George Jones (STUDENT NUMBER), Jamie Mills (16004255)
+ * @author
+ * Adwait Chhetri (STUDENT NUMBER),
+ * George Jones (STUDENT NUMBER),
+ * Jamie Mills (16004255)
  */
 public class UWEAS {
 
-    List<Hall> halls;
-    List<User> users;
-    List<Application> applications;
+    // Using list interface (cannot instantiate)
+    private static List<Hall> halls;
+    private static List<User> users;
+    private static List<Application> applications;
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        // Instantiate main lists
+        halls = new ArrayList<Hall>();
+        users = new ArrayList<User>();
+        applications = new ArrayList<Application>();
     }
     
+    // Adds an unique hall to the hall list
     public void addHall(Hall hall)
     {
         if (halls.contains(hall) == false)
@@ -28,6 +39,7 @@ public class UWEAS {
         }
     }
     
+    // Removes an existing hall from the hall list
     public void removeHall(Hall hall)
     {
         if (halls.contains(hall))
@@ -36,6 +48,7 @@ public class UWEAS {
         }
     }
     
+    // Adds an unique user to the user list
     public void addUser(User user)
     {
         if (users.contains(user) == false)
@@ -44,6 +57,7 @@ public class UWEAS {
         }
     }
     
+    // Removes an existing user from the user list
     public void removeUser(User user)
     {
         if (users.contains(user))
@@ -52,6 +66,7 @@ public class UWEAS {
         }
     }
     
+    // Adds an unique application to the application list
     public void addApplication(Application application)
     {
         if (applications.contains(application) == false)
@@ -60,6 +75,7 @@ public class UWEAS {
         }
     }
     
+    // Removes an existing application from the application list
     public void removeApplication(Application application)
     {
         if (applications.contains(application))
@@ -68,16 +84,19 @@ public class UWEAS {
         }
     }
     
+    // Returns byref the hall list
     public List<Hall> getHalls()
     {
         return halls;
     }
     
+    // Returns byref the user list
     public List<User> getUsers()
     {
         return users;
     }
     
+    // Returns byref the application list
     public List<Application> getApplications()
     {
         return applications;
